826e7cab16d90a9841859c9bd40314ff1f89bd50
FizaCodeCraft <ghulamfizzaabbasi@gmail.com>
2024-09-28 23:07:48 +0500

    Update script.js

--- milestone 1/script.js
+++ milestone 1/script.js
@@ -1,13 +1,4 @@
-/*const togglebutton = document.getElementById('toggle-skills') as HTMLButtonElement
-const skill = document.getElementById('skills') as HTMLElement
 
-togglebutton.addEventListener('click', ()=>{
-    if(skill.style.display === 'none') {
-        skill.style.display = 'block'
-    } else{
-        skill.style.display = 'none'
-    }
-})*/
 // Get the button and the skills section
 var toggleSkillsButton = document.getElementById('toggle-skills');
 var skillsSection = document.getElementById('skills');
